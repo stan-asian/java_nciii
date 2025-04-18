8f8714eacc99609a9dff31404f07eabb37eaf44c
stan-asian <stanleypanag18@gmail.com>
2025-04-18 19:02:50 +0000

    third commit

--- Encapsulation/Person.java
+++ Encapsulation/Person.java
@@ -4,14 +4,32 @@ public class Person {
     
     private String fName;
     private String lName;
-    private String fullName = fName + lName;
+    private String fullName;
 
     public Person(String fName, String lName){
         this.fName = fName;
         this.lName = lName;
+        this.fullName = this.fName + " " + this.lName;
     }
 
     public String getFullName(){
         return this.fullName;
     }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
 }
